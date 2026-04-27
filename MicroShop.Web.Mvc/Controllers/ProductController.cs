27037f65a01fb8cf1724a3f37226519e861d16f2
mahdi amini <classicus.ma@gmail.com>
2026-04-27 13:13:08 +0330

    Added Product Views.

--- MicroShop.Web.Mvc/Controllers/ProductController.cs
+++ MicroShop.Web.Mvc/Controllers/ProductController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MicroShop.Web.Mvc.Services.ProductServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MicroShop.Web.Mvc.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly IProductService productService;
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var products = productService.GetAllProduct();
+            return View(products);
+        }
+
+        public IActionResult Details(Guid id)
+        {
+            var product = productService.Getproduct(id);
+            return View(product);
         }
     }
 }
