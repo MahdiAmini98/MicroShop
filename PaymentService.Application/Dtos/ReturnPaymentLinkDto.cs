ec11ef4cbc9684bd6460a032b4635753bd93ea0d
mahdi amini <classicus.ma@gmail.com>
2026-05-06 20:12:35 +0330

    Added ZarinPal Package To Payment Service.

---
+++ PaymentService.Application/Dtos/ReturnPaymentLinkDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentService.Application.Dtos
+{
+    public class ReturnPaymentLinkDto
+    {
+        public string PaymentLink { get; set; }
+    }
+}
---
+++ PaymentService.Application/Dtos/VerificationPayResultDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentService.Application.Dtos
+{
+    public class VerificationPayResultDto
+    {
+        public int Status { get; set; }
+        public long RefID { get; set; }
+    }
+}
